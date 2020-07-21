37bd4c277cf51b18526c2d89f88fd1b8d0e61d12
cbarros7 <cj.barros07@gmail.com>
2020-07-21 11:37:20 -0500

    Fix betty task 0 - 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,22 +1,22 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left -  inserts a node as the left-child 
+ * binary_tree_insert_left -  inserts a node as the left-child
  * @parent: A pointer to the node to insert the letf-child in
  * @value: The value to store in the new node
- * Return: new node 
+ * Return: new node
  */
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
-	
+
 	if (parent == NULL)
-		return NULL;
+		return (NULL);
 
 	new_node = binary_tree_node(parent, value);
 	if (new_node == NULL)
-		return NULL;
+		return (NULL);
 
 	if (parent->left != NULL)
 	{
@@ -25,6 +25,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	}
 	parent->left = new_node;
 
-	return new_node; 
+	return (new_node);
 }
 
