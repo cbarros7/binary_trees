3226203a1b79902ea854355ddd22d52207b09157
ymcastellar <yoycas@hotmail.com>
2020-07-22 14:52:28 -0500

    added files for task 15

--- binary_trees.h
+++ binary_trees.h
@@ -2,7 +2,6 @@
 #define BINARY_TREES_H
 
 #define MAX(a, b)  (a < b ? b : a)
-#define DIFF(a, b) (a - b)
 
 #include <stddef.h>
 #include <stdio.h>
@@ -50,5 +49,6 @@ size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
+int binary_tree_is_full(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
